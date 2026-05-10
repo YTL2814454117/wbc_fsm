6bed05df5c76f6195075ea57961ef66a32fd4f47
Wannacats <2814454117@qq.com>
2026-05-10 11:46:33 +0800

    减少舞蹈过渡时间

--- include/FSM/State_Amp.h
+++ include/FSM/State_Amp.h
@@ -66,7 +66,7 @@ private:
     bool _high_speed_mode = false;
     bool _auto_loco_after_enter_requested = false;
     bool _auto_loco_active = false;
-    double _auto_loco_delay = 3.0;
+    double _auto_loco_delay = 1.0;
     double _auto_loco_elapsed = 0.0;
     int _auto_loco_last_print_second = -1;
 
